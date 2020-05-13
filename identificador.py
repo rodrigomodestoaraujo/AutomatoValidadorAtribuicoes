9c80de07dba19c52fe8acd12d5d0c0b0f4cdf148
Caio <caio.student@outlook.com>
2020-05-13 18:20:20 -0300

    Refatoração, comentarios e documentação

--- identificador.py
+++ identificador.py
@@ -1,79 +1,81 @@
+''' CASO:
+Modelar e implementar um autômato que faça valicação de atribuições, essa atribuição dese ter a seguinte sintaxe:
+
+Sintaxe:  INDENTIFICAR OP_ATRIB ( IDENTIFICADOR | NÚMERO ) ( OP_ATRIB ( IDENTIFICADOR | NÚMERO )  )*  PV
+
+Sendo que:
+
+INDENTIFICAR   = Inicia por uma letra minúscula, seguido por uma quantidade qualquer de letras minúsculas, dígitos
+ou underline.
+
+OP_ATRIB       = Representa o caractere   '='
+
+NÚMERO         = inicia por um dígito, seguido por uma quantidade qualquer de dígitos, podendo ter opcionalmente
+a parte fracionária. Caso ele tenha a parte fracionária, ele recebe o caractere ‘.’ , seguido de uma sequência
+de um ou mais dígitos.
+
+OP_ARIT        = Representa os caracteres ( '+' | '-' | '*' |'/' )
+
+PV             = Representa o caractere   ';'
+'''
+
+# IDENTIFICADOR: [a-z] ( [a-z] | [0-9] | [_] )*
+# NÚMERO: ( [0-9]([0-9])* ) | ( [0-9] ([0-9])* '.' [0-9]([0-9])* )
+
+
+#FUNÇÃO PARA VALIDAR UM IDENTIFICADOR
 def identificador(string):
-    string = list(string)
-    validade = False
-    if (((ord(string[0]) > 96) and (ord(string[0]) < 123)) and (ord(string[0]) != 95)):
-        for i in range(len(string)):
-            for j in range (97, 123):
-                if ((ord(string[i]) > 96 and ord(string[i]) < 123) or (ord(string[i]) > 47 and ord(string[i]) < 58) or (ord(string[i]) == 95)):
-                    validade = True;
-                    break;
-                else:
-                    validade = False;
-                    break;
-            if (validade == False):
-                break;
-    return validade
-
-def numero(numero): 
-    numero = list(numero)
-    validade = False
-    if (numero.count('.') > 1): 
-        return validade
-    if (numero[-1] == '.'):
-        return False 
-    if (((ord(numero[0]) > 47 and ord(numero[0]) < 58))):
-        for i in range(len(numero)):
-            for j in range (47, 58):
-                if ((ord(numero[i]) > 47 and ord(numero[i]) < 58) or (ord(numero[i]) == 46)): 
-                    validade = True;
-                    break;
-                else:
-                    validade = False
-                    break
-            if (validade == False):
-                break;  
-    return validade
-
-def main(string): 
+    inicial = ord(string[0])
+    if not( inicial > 96 and inicial < 123 ):
+        return False
     
-    string = string.replace(" ", "")
-    
-    if (string.count("//") > 0 or string.count("**") > 0 or string.count("++") > 0 or string.count("--") > 0):
-        return ('Automato não validado')
+    for caracter in string:
+        decimal = ord( caracter )
+        if not( ( decimal > 96 and decimal < 123 ) or (decimal > 47 and decimal < 58) or (decimal == 95)):
+            return False
+    return True
+
+#FUNÇÃO PRA VALIDAR SEQUÊNCIA DE NÚMERO
+# ( [0-9]([0-9])* ) | ( [0-9] ([0-9])* '.' [0-9]([0-9])* )
+def numero(string):
+    if (string[0] == '.' or string[-1] == '.' or string.count('.') > 1):
+        return False
+        
+    for caracter in string:
+        decimal = ord( caracter )
+        if not( ( decimal > 47 and decimal < 58 ) or decimal == 46):
+            return False
+    return True
+
+#FUNÇÃO PRA VALIDAR SE A PALAVRA SE O PADRÃO
+def validador(string):
+    if (string.count(' ') > 0 or string.count('//') > 0 or string.count('**') > 0 or string.count('++') > 0 or string.count('--') > 0):
+        return False
+    if( (string == '' ) or (string == ';') or (string[-1] != ';') ):
+        return False
 
     string = string.replace("+",",")
     string = string.replace("-",",")
     string = string.replace("*",",")
     string = string.replace("/",",")
-    
-    if ((string)) == ("") :
-        return ('Automato não validado')
-    
-    if (string == ';'):
-        return ('Automato não validado')
-
-    if (string[-1] != ';'):
-        return ('Automato não validado')
-    
     string = string.replace(";","")
-    
-    string = string.split(",")
-    
-    for i in range (len(string)): 
-        if (identificador(string[i]) == False and numero((string[i])) == False):
-            return ('Automato não validado')
+    lista = string.split(",")
+   
+    return lista
+
+def main( string ):
+    lista = validador(string)
+    if not lista:
+        print( "\nWARN: Fora do padrão" )
+        return False
         
-    return ('Automato validado')
+    for palavra in lista:
+        if( identificador( palavra ) == False and numero( palavra ) == False):
+            print( "\nWARN: Fora do padrão" )
+            return False
+    print('\nEssa palavra é Válida')
 
+while(True):
+    main(input(str('\nDigite uma palavra: ')))
 
-print(main('merecemos + uma + carta + de + recomentacao + pelo + trabalho + 10;'))
-    
-
-    
-
-
-
-        
-    
-    
 
